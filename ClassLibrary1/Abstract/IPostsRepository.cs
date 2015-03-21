25396418c7aee771cc80d6de16592047900c5f97
Cale Davis <daviscale@gmail.com>
2015-03-21 08:14:20 -0400

    Added IPostsRepository

---
+++ ClassLibrary1/Abstract/IPostsRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using CaleBlog.Domain.Entities;
+
+namespace CaleBlog.Domain.Abstract
+{
+    public interface IPostsRepository
+    {
+        IEnumerable<Post> Posts { get; }
+    }
+}
