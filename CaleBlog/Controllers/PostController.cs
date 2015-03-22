698d14562884513220d41dbb3fe9b64a23557444
Cale Davis <daviscale@gmail.com>
2015-03-21 23:05:17 -0400

    Initial implementation of PostController

---
+++ CaleBlog/Controllers/PostController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using CaleBlog.Domain.Abstract;
+using CaleBlog.Domain.Entities;
+
+namespace CaleBlog.Controllers
+{
+    public class PostController : Controller
+    {
+        private IPostsRepository repository;
+
+        public PostController(IPostsRepository postRepository)
+        {
+            this.repository = postRepository;
+        }
+
+        public ViewResult List()
+        {
+            return View(repository.Posts);
+        }
+    }
+}
