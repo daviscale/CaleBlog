5335e09e7f85f21666f2d1805a27cc00b4de8505
Cale Davis <daviscale@gmail.com>
2015-03-22 00:12:59 -0400

    Added EFDbContext file with Posts property

---
+++ ClassLibrary1/Concrete/EFDbContext.cs
@@ -0,0 +1,11 @@
+﻿using System.Data.Entity;
+
+using CaleBlog.Domain.Entities;
+
+namespace CaleBlog.Domain.Concrete
+{
+    public class EFDbContext : DbContext
+    {
+        public DbSet<Post> Posts { get; set; }
+    }
+}
