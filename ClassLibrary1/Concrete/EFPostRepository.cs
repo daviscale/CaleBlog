bdaa4956658aa2cd5164225f72d1e06d62b5ba02
Cale Davis <daviscale@gmail.com>
2015-03-22 00:27:23 -0400

    Added EFPostRepository

---
+++ ClassLibrary1/Concrete/EFPostRepository.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+using CaleBlog.Domain.Abstract;
+using CaleBlog.Domain.Entities;
+
+namespace CaleBlog.Domain.Concrete
+{
+    public class EFPostRepository : IPostsRepository
+    {
+        private EFDbContext context = new EFDbContext();
+
+        public IEnumerable<Post> Posts
+        {
+            get { return context.Posts;  }
+        }
+    }
+}
