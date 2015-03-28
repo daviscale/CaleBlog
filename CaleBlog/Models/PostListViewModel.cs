a4cf9825d68a88eee0298500cb291b7b6e4c48fd
Cale Davis <daviscale@gmail.com>
2015-03-28 08:21:03 -0400

    added PostListViewModel that includes Posts and PagingInfo

---
+++ CaleBlog/Models/PostListViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+using CaleBlog.Domain.Entities;
+
+namespace CaleBlog.WebUI.Models
+{
+    public class PostListViewModel
+    {
+        public IEnumerable<Post> Posts { get; set; }
+        public PagingInfo PagingInfo { get; set; }
+    }
+}
