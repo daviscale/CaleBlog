a4941d8b032d9c9119171ec7caff21b96a3c7422
Cale Davis <daviscale@gmail.com>
2015-03-29 08:53:23 -0400

    use RESTful routing of post listings

--- CaleBlog/App_Start/RouteConfig.cs
+++ CaleBlog/App_Start/RouteConfig.cs
@@ -14,6 +14,16 @@ namespace CaleBlog.WebUI
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: null,
+                url: "Page{page}",
+                defaults: new
+                {
+                    Controller = "Post",
+                    action = "List"
+                }
+            );
+            
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Post", action = "List", id = UrlParameter.Optional }
