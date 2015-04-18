71966a8abb37106a5df674766bd28ad2c096e72f
Cale Davis <daviscale@gmail.com>
2015-04-18 11:00:38 -0400

    post id is hidden input

--- ClassLibrary1/Entities/Post.cs
+++ ClassLibrary1/Entities/Post.cs
@@ -4,11 +4,13 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.Mvc;
 
 namespace CaleBlog.Domain.Entities
 {
     public class Post
     {
+        [HiddenInput(DisplayValue = false)]
         public int PostID { get; set; }
 
         public string Title { get; set; }
