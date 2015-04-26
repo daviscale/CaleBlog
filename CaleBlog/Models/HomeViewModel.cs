6dc88d98a2a4862a839ce344976da15915956467
Cale Davis <daviscale@gmail.com>
2015-04-26 10:47:34 -0400

    added HomeViewModel

---
+++ CaleBlog/Models/HomeViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using CaleBlog.Domain.Entities;
+
+namespace CaleBlog.WebUI.Models
+{
+    public class HomeViewModel
+    {
+        public IEnumerable<Post> RecentPosts { get; set; }
+        public Post HomePost { get; set; }
+    }
+}
